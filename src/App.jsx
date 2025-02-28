ac235df917aa882d7b89fec2fd53176577753836
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2025-03-01 00:23:30 +0530

    added h1 tag for instructions

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ export default function App() {
         className="w-full h-screen duration-100 "
         style={{ backgroundColor: bgColor }}
       >
+        <h1 className="flex text-slate-400 text-3xl justify-center h-screen items-center pb-4 shadow-lg">
+          CLICK ON THE BUTTON BELOW TO CHANGE COLORS
+        </h1>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
           <ButtonCard bgColor={bgColor} setbgColor={setbgColor} />
           {/* <div className="flex flex-wrap justify-center gap-4 shadow-lg bg-white text-black px-3 py-3 rounded-3xl">
